e10800d3d1c96a8d7d79cbb4b18510875620b3f5
Krzysztof Nikiel <emctrakers@gmail.com>
2020-04-19 10:51:55 +0200

    new files

---
+++ src/DependencyInjection/KrzysztofNikielRecruitmentTaskBundleExtension.php
@@ -0,0 +1,22 @@
+<?php
+namespace KrzysztofNikiel\Bundle\RecruitmentTaskBundl\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class KrzysztofNikielRecruitmentTaskBundleExtension extends Extension
+{
+
+    /**
+     * @param array $configs
+     * @param ContainerBuilder $container
+     * @throws \Exception
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(dirname(__DIR__).'/Resources/config'));
+        $loader->load('services.yaml');
+    }
+}
